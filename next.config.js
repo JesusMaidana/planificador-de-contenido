2726cd52e66c7a1889bf6709f7e8d20d6fe2d1c9
Jesus Maidana <jesusmaidana35@gmail.com>
2025-12-27 21:18:11 -0300

    chore: Remove experimental turbo mode configuration from Next.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        turbo: false, // ← esto se elimina
-    },
     webpack(config) {
         return config
     },
